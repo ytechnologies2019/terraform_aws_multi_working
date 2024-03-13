27c548d3c5439f3f5d229c351d5a983ac630632f
thihathura <private.ytechnologies@gmail.com>
2024-03-13 20:03:00 +0630

    .

--- main.tf
+++ main.tf
@@ -1,22 +1,16 @@
-# add single file
-resource "git_add" "single_file" {
-  directory = "/Users/y/Desktop/terraform_aws_multi_working/main.tf"
-  add_paths = ["."]
+resource "terraform_data" "example1" {
+  provisioner "local-exec" {
+    command = "git add ."
+  }
+}
+resource "terraform_data" "example2" {
+  provisioner "local-exec" {
+    command = "git commit -am ."
+  }
 }
 
-# commit changes and supply message
-resource "git_commit" "commit" {
-  directory = "/Users/y/Desktop/terraform_aws_multi_working/main.tf"
-  message   = "commited"
-}
-
-resource "git_push" "push" {
-  directory = "/Users/y/Desktop/terraform_aws_multi_working/main.tf"
-  refspecs  = ["refs/heads/master:refs/heads/master"]
-#   remote = "git@github.com:ytechnologies2019/terraform_aws_multi_working.git"
-    auth = {
-    ssh_key = {
-      private_key_path = pathexpand("~/.ssh/id_rsa")
-    }
+resource "terraform_data" "example3" {
+  provisioner "local-exec" {
+    command = "git push"
   }
 }
