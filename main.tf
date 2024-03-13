6812b0b630a41967698abee184aa10119946ad81
thihathura <private.ytechnologies@gmail.com>
2024-03-13 19:29:01 +0630

    main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,22 @@
 # add single file
 resource "git_add" "single_file" {
-  directory = "."
+  directory = "/Users/y/Desktop/terraform_aws_multi_working/main.tf"
   add_paths = ["."]
 }
 
 # commit changes and supply message
 resource "git_commit" "commit" {
-  directory = "."
+  directory = "/Users/y/Desktop/terraform_aws_multi_working/main.tf"
   message   = "commited"
 }
 
 resource "git_push" "push" {
-  directory = "."
-  refspecs  = ["."]
+  directory = "/Users/y/Desktop/terraform_aws_multi_working/main.tf"
+  refspecs  = ["refs/heads/master:refs/heads/master"]
+#   remote = "git@github.com:ytechnologies2019/terraform_aws_multi_working.git"
+    auth = {
+    ssh_key = {
+      private_key_path = pathexpand("~/.ssh/id_rsa")
+    }
+  }
 }
